85b627416b97ae3e7d8755be6174ea6473cec3e7
nomadbard916 <neonnova916@gmail.com>
2020-09-13 19:06:58 +0800

    added middleware "morgan"

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
 // routes
 const { getPosts } = require('./routes/post');
+
+// middleware
+app.use(morgan('dev'));
 
 app.get('/', getPosts);
 
