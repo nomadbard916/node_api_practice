18f3a4e4d64b7c2716241626dbe46ae179667ee4
nomadbard916 <neonnova916@gmail.com>
2020-09-13 18:59:58 +0800

    separating routes

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
+// routes
+const { getPosts } = require('./routes/post');
 
-app.get('/', (req, res) => {
-  res.send('Hello world from node js');
-});
+app.get('/', getPosts);
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!');
